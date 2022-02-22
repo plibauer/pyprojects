e63f5371fbe3f2fcb0b85cf5833cb7e68e41860f
plibauer <plibauer@gmail.com>
2022-02-22 15:35:35 +1100

    Added new exercise 3 - linear regression

--- scikit-learn_machine_learning_in_Python.py
+++ scikit-learn_machine_learning_in_Python.py
@@ -69,6 +69,44 @@ Exercise 2: Introduction to scikit-learn estimator object
     print(model)
 
 #--------------------------------------------------------------------------------
+
+def ex_3(descr=False):
+
+    if descr:
+        return """
+Exercise 3: Simple Linear Regression exercise
+=============================================
+
+  Plot a basic example of fitting using Linear Regresssion
+"""
+    from sklearn.linear_model import LinearRegression
+
+    # x from 0 to 30
+    x = 30 * np.random.random((20, 1))
+
+    # y = a*x + b with noise
+    y = 0.5 * x + 1.0 + np.random.normal(size=x.shape)
+
+    # create a linear regression model
+    model = LinearRegression()
+    model.fit(x, y)
+
+    # predict y from the data
+    x_new = np.linspace(0, 30, 100)
+    y_new = model.predict(x_new[:, np.newaxis])
+
+    # plot the results
+    plot.figure(figsize=(4, 3))
+    ax = plot.axes()
+    ax.scatter(x, y)
+    ax.plot(x_new, y_new)
+
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.axis('tight')
+    
+    plot.show()
+
 #--------------------------------------------------------------------------------
 #--------------------------------------------------------------------------------
 #--------------------------------------------------------------------------------
